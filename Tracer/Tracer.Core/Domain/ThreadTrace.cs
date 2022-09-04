c406fab75aa747bd24a20a6dc97c29c645824d70
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-05 00:06:28 +0300

    Added thread trace and it`s tests.

---
+++ Tracer/Tracer.Core/Domain/ThreadTrace.cs
@@ -0,0 +1,35 @@
+﻿namespace Tracer.Core.Domain;
+
+public class ThreadTrace
+{
+    private readonly Stack<MethodTrace> _stackOfMethods;
+
+    private readonly List<MethodTrace> _allMethods;
+
+    public ThreadTrace()
+    {
+        _stackOfMethods = new Stack<MethodTrace>();
+        _allMethods = new List<MethodTrace>();
+    }
+
+    public long ElapsedTime => _allMethods.Select(item => item.Elapsed.Milliseconds).Sum();
+
+    public void StartTrackMethod(MethodTrace methodTrace)
+    {
+        if (_stackOfMethods.Count == 0)
+        {
+            _allMethods.Add(methodTrace);
+        }
+        else
+        {
+            _stackOfMethods.Peek().AddNestedMethod(methodTrace);
+        }
+        _stackOfMethods.Push(methodTrace);
+    }
+
+    public void StopTrackMethod()
+    {
+        _stackOfMethods.Pop().StopTrackTime();
+    }
+
+}
