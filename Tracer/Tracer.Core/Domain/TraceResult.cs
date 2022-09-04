a695346384f8060a5ae4d09f3b295e26f363ffdb
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-04 17:27:33 +0300

    Added Trace Result Dto and it`s tests.

---
+++ Tracer/Tracer.Core/Domain/TraceResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Tracer.Core.Domain;
+
+public class TraceResult
+{
+    public IReadOnlyDictionary<int, ThreadTrace> ThreadTraces { get; }
+    
+    public TraceResult(IDictionary<int, ThreadTrace> threadsTraces)
+    {
+        ThreadTraces = new ReadOnlyDictionary<int, ThreadTrace>(threadsTraces);
+    }
+    
+}
