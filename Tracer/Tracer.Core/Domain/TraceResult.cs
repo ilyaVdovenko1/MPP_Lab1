d0e7f1cd28514053059eb6d2b382676836c49a67
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-16 02:14:38 +0300

    Changed trace result contract.

--- Tracer/Tracer.Core/Domain/TraceResult.cs
+++ Tracer/Tracer.Core/Domain/TraceResult.cs
@@ -4,11 +4,16 @@ namespace Tracer.Core.Domain;
 
 public class TraceResult
 {
-    public IReadOnlyDictionary<int, ThreadTrace> ThreadTraces { get; }
+    public IReadOnlyDictionary<int, ThreadTrace> ThreadTraces { get; set; }
     
     public TraceResult(IDictionary<int, ThreadTrace> threadsTraces)
     {
         ThreadTraces = new ReadOnlyDictionary<int, ThreadTrace>(threadsTraces);
+    }
+
+    public TraceResult()
+    {
+        
     }
     
 }
